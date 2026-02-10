95f572a367d3bee63bc1940f902aa4d0f8db7645
Andres Pasuin <candrespausin2001@gmail.com>
2026-02-10 15:04:51 +0100

    add: Created a view for Services

---
+++ app/services/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <section className="flex flex-col justify-center items-center gap-5 mt-100">
+      <h1 className="font-extrabold text-white text-8xl">SERVICIOS</h1>
+    </section>
+  );
+}
