4fef2e99a4eae7d7edd2ded66e01702536af6e7e
Andres Pasuin <candrespausin2001@gmail.com>
2026-02-12 18:28:11 +0100

    add: Hnadle data from Feature section from a different file

---
+++ sections/features/feature.data.ts
@@ -0,0 +1,17 @@
+export const featureData = [
+  {
+    id: 1,
+    title: "Rigor técnico",
+    description: "Diseños y cálculos estructurales precisos, optimizando cada recurso de su proyecto."
+  },
+  {
+    id: 2,
+    title: "Seguridad certificada",
+    description: "Sistemas de acceso que cumplen estrictamente con normativas ANSI/OSHA para garantizar cero accidentes."
+  },
+  {
+    id: 3,
+    title: "Eficiencia en gestión",
+    description: "Proyectos entregados en tiempo y forma mediante metodologías avanzadas de Project Management."
+  }
+]
