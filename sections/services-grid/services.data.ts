f00fb1bab20260260fac841f9eaf1e7dd6c2bdbf
Andres Pasuin <candrespausin2001@gmail.com>
2026-02-12 18:33:38 +0100

    add: Added the service data file

---
+++ sections/services-grid/services.data.ts
@@ -0,0 +1,20 @@
+export const servicesData = [
+  {
+    title: "Ingeniería y Consultoría",
+    description: "Modelado 3D, cálculos estructurales y memorias técnicas.",
+    image: "/ing-referencia.png",
+    link: "/servicios#ingenieria"
+  },
+  {
+    title: "Sistemas de Andamiaje",
+    description: "Suministro y montaje certificado bajo normativa ANSI/OSHA.",
+    image: "/andamios.webp",
+    link: "/servicios#andamios"
+  },
+  {
+    title: "Project Management",
+    description: "Supervisión técnica y control operativo integral de obra.",
+    image: "/pm.jpg",
+    link: "/servicios#gestion"
+  },
+];
