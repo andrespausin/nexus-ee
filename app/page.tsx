69912786e1973748f75fbb9acf12fe8dcfd63786
Andres Pasuin <candrespausin2001@gmail.com>
2026-03-15 20:34:16 +0100

    edit: Commented the PartnerSection

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <Features />
       <SafetyBanner />
       <ServicesGrid />
-      <PartnersSection />
+      {/* <PartnersSection /> */}
       <ContactSection />
     </section>
   );
