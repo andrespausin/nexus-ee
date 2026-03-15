ce0d6a744a37e1bfe07d81c9a2bad813b848b042
Andres Pasuin <candrespausin2001@gmail.com>
2026-03-15 20:40:27 +0100

    edit: Modified the content

--- sections/services-grid/services.data.ts
+++ sections/services-grid/services.data.ts
@@ -1,20 +1,20 @@
 export const servicesData = [
   {
-    title: "Ingeniería, Arquitectura y Consultoría",
-    description: "Diseño integral, modelado BIM y cálculos estructurales avanzados para garantizar la viabilidad técnica de su proyecto.",
+    title: "Arquitectura, Ingeniería Construcción",
+    description: "Desarrollamos estudios técnicos, anteproyectos y proyectos ejecutivos mediante soluciones de ingeniería y arquitectura. <br/><br/>Analizamos la viabilidad técnica y económica, así como la correcta interpretación y aplicación de normas de construcción, garantizando le entrega de proyectos eficientes, seguros y sostenibles.",
     image: "/ing-referencia.png",
     link: "/services/engineering"
   },
   {
     title: "Gestión de proyectos",
-    description: "Gestión estratégica y representación técnica independiente para optimizar recursos y asegurar el cumplimiento de plazos.",
-    image: "/pm.jpg",
+    description: "Proporcionamos gestión integral y representación técnica independiente del cliente durante todas las fases del proyecto. Coordinamos equipos, controlamos costes y plazos, y garantizamos el cumplimiento técnico, normativo y de calidad.",
+    image: "/pro-mng.jpg",
     link: "/services/project-management"
   },
   {
     title: "Ejecución de obras civiles",
     description: "Construcción de infraestructura técnica y sistemas de acceso seguro bajo los más estrictos estándares de calidad y seguridad.",
-    image: "/andamios.webp",
+    image: "/andamios.jpeg",
     link: "/services/civil-engineer"
   },
 ];
