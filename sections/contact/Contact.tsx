375e91a9b26416e6a56054e8c9069a38e8d38be7
Andres Pasuin <candrespausin2001@gmail.com>
2026-03-15 20:37:21 +0100

    edit: Modified the size of label text

--- sections/contact/Contact.tsx
+++ sections/contact/Contact.tsx
@@ -1,4 +1,5 @@
 import ContactForm from "./component/ContactForm";
+import { FaWhatsapp } from "react-icons/fa";
 
 const ContactSection = () => {
   return (
@@ -19,26 +20,38 @@ const ContactSection = () => {
             <div className="p-10 bg-white/3 backdrop-blur-xl border border-white/10 rounded-[2.5rem] h-full flex flex-col justify-between">
               <div className="space-y-12">
                 <div>
-                  <h3 className="text-blue-500 font-mono text-[10px] uppercase tracking-[0.3em] mb-6 flex items-center gap-2">
+                  <h3 className="text-blue-500 font-mono  text-[12px] lg:text-[15px] uppercase tracking-[0.3em] mb-6 flex items-center gap-2">
                     <span className="w-1.5 h-1.5 bg-blue-500 rounded-full animate-pulse" />
                     Localización Física
                   </h3>
                   <p className="text-white lg:text-2xl font-bold tracking-tight">
-                    Av. de la Castellana, 14 <br />
-                    <span className="text-gray-500">Madrid, España.</span>
+                    Calle de las Huertas Nº14, Esc 2, Planta Nº2, Puerta B <br />
+                    Paracuellos de Jarama, Madrid
                   </p>
                 </div>
 
                 <div>
-                  <h3 className="text-blue-500 font-mono text-[10px] uppercase tracking-[0.3em] mb-6">
+                  <h3 className="text-blue-500 font-mono  text-[12px] lg:text-[15px] uppercase tracking-[0.3em] mb-6">
                     Enlace directo
                   </h3>
                   <div className="space-y-2 lg:text-2xl">
+                    <div className="flex flex-col">
+                      <a
+                        href="https://wa.me/34624951014"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center gap-2 text-white font-bold hover:text-blue-400 transition-colors"
+                      >
+                        <FaWhatsapp />
+                        +34 624 95 10 14
+                      </a>
+                    </div>
                     <p className="text-white font-bold hover:text-blue-400 transition-colors cursor-pointer">
-                      +XX XXX XXX XXX
-                    </p>
-                    <p className="text-gray-400 text-lg hover:text-blue-400 transition-colors cursor-pointer">
-                      administracion@nexusee.com
+                      <a href="mailto:administracion@nexusee.com"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        administracion@nexusee.com
+                      </a>
                     </p>
                   </div>
                 </div>
